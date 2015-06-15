993087162d1e8b33c18fc5c3d236c6c495f88a0a
fzed51 <fzed51@yahoo.fr>
2015-06-15 02:57:49 +0200

    :up: correction du chemin du webroot

--- serveur-routeur.php
+++ serveur-routeur.php
@@ -25,42 +25,42 @@
  */
 
 if (php_sapi_name() == 'cli-server') {
-    $fullNamePublic = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
-    if (is_file($fullNamePublic)) {
-        $path = pathinfo($fullNamePublic);
-        switch ($path["extension"]) {
-            case 'ico':
-                header("Content-Type: image/x-icon");
-                break;
-            case 'css':
-                header("Content-Type: text/css");
-                break;
-            case 'js':
-                header("Content-Type: application/javascript");
-                break;
-            case 'json':
-                header("Content-Type: application/json");
-                break;
-            case 'xml':
-                header("Content-Type: application/xml");
-                break;
-            case 'jpg':
-                header("Content-Type: image/jpeg");
-                break;
-            case 'png':
-                header("Content-Type: image/png");
-                break;
-            case 'gif':
-                header("Content-Type: image/gif");
-                break;
-            case 'svg':
-                header("Content-Type: image/svg+xml");
-                break;
-            default:
-                return FALSE;
-        }
-        readfile($fullNamePublic);
-    } else {
-        require "./core.php";
-    }
+	$fullNamePublic = $_SERVER["DOCUMENT_ROOT"] . '\public' . $_SERVER["REQUEST_URI"];
+	if (is_file($fullNamePublic)) {
+		$path = pathinfo($fullNamePublic);
+		switch ($path["extension"]) {
+			case 'ico':
+				header("Content-Type: image/x-icon");
+				break;
+			case 'css':
+				header("Content-Type: text/css");
+				break;
+			case 'js':
+				header("Content-Type: application/javascript");
+				break;
+			case 'json':
+				header("Content-Type: application/json");
+				break;
+			case 'xml':
+				header("Content-Type: application/xml");
+				break;
+			case 'jpg':
+				header("Content-Type: image/jpeg");
+				break;
+			case 'png':
+				header("Content-Type: image/png");
+				break;
+			case 'gif':
+				header("Content-Type: image/gif");
+				break;
+			case 'svg':
+				header("Content-Type: image/svg+xml");
+				break;
+			default:
+				return FALSE;
+		}
+		readfile($fullNamePublic);
+	} else {
+		require "./core.php";
+	}
 }
